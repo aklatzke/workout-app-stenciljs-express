bb2a89187cd5d53aefbf1f97d2613668af70ee97
Andrew Klatzke <andrewklatzke@gmail.com>
2018-07-30 00:00:59 -0500

    mlab:

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,3 +1,3 @@
 import mongo from 'then-mongo';
 
-export default mongo(process.env.MONGODB_URI || 'workout', ['workouts', 'users', 'exercises', 'exerciseReference']);
+export default mongo("mongodb://heroku_57j9k2hn:ulu7mj5rb4hme068q5fmabm78m@ds259351.mlab.com:59351/heroku_57j9k2hn", ['workouts', 'users', 'exercises', 'exerciseReference']);
